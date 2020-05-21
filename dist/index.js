4119805fee0517cd5196e4b891f3c7cedcde0f9c
swist <me@swistofon.pl>
2020-05-21 21:29:01 +0100

    Make the action extract the branch name

--- dist/index.js
+++ dist/index.js
@@ -511,19 +511,16 @@ async function run() {
     const github_token = core.getInput("GITHUB_TOKEN");
 
     const context = github.context;
-    core.setFailed(JSON.stringify(context));
-    if (context.payload.issue.pull_request == null) {
-      core.setFailed("No issue request found.");
-      return;
-    }
-    const issue_number = context.payload.issue.number;
+    const pull_number = context.payload.issue.number;
 
     const octokit = new github.GitHub(github_token);
-    const new_comment = octokit.issues.createComment({
+    const pullRequest = await octokit.pulls.get({
       ...context.repo,
-      issue_number: issue_number,
-      body: comment
+      pull_number
     });
+    const branchName = `${pullRequest.head.ref}-${pullRequest.number}-${context.run_number}`;
+    core.setOutput("test-head", branchName);
+    core.setOutput("pr-head", pullRequest.head.ref);
   } catch (error) {
     core.setFailed(error.message);
   }
